c1b81531b371d6156d45e6b3fc8b3bd8eb19a513
Mingyuan <mingyuanwang1990@gmail.com>
2024-10-31 16:31:33 +0800

    Update sbatchTemplatePython.sh

--- sbatchTemplatePython.sh
+++ sbatchTemplatePython.sh
@@ -51,6 +51,8 @@ pip3 install numpy
 pip3 install torch
 pip3 install scipy
 pip3 install pillow
+pip3 install torchvision tqdm opencv-python-headless matplotlib albumentations gradio
+
 
 # Submit your job to the cluster
 srun --gres=gpu:1 python3 /common/home/projectgrps/CS701/CS701G8/gitcodes/SAM-Med2D/test.py
