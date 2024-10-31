593ad96af10b072f01bf38352b81c0ed88a112b7
Mingyuan <mingyuanwang1990@gmail.com>
2024-10-31 16:33:17 +0800

    Update sbatchTemplatePython.sh

--- sbatchTemplatePython.sh
+++ sbatchTemplatePython.sh
@@ -51,7 +51,12 @@ pip3 install numpy
 pip3 install torch
 pip3 install scipy
 pip3 install pillow
-pip3 install torchvision tqdm opencv-python-headless matplotlib albumentations gradio
+pip3 install torchvision
+pip3 install tqdm
+pip3 install opencv-python-headless
+pip3 install matplotlib
+pip3 install albumentations
+pip3 install gradio
 
 
 # Submit your job to the cluster
